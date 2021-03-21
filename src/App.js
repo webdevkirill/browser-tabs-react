59b57ede160ceda215b687a357f5ffcc85968e8e
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-03-21 13:55:33 +0300

    Добавляет верстку и стили

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<div className='browser'>
+				<div className='tabs'>
+					<div className='tab'>
+						<a>Главная</a>
+					</div>
+					<div className='tab'>
+						<a>О нас</a>
+					</div>
+					<div className='tab'>
+						<a>Преимущеста</a>
+					</div>
+				</div>
+
+				<div className='viewport'>Pages Go Here</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
